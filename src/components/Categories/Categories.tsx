9545f00d3f88da427935a83c86a79281ee4a80d8
VladoSimic23 <57799129+VladoSimic23@users.noreply.github.com>
2023-02-02 14:13:57 +0100

    Update Categories.tsx

--- src/components/Categories/Categories.tsx
+++ src/components/Categories/Categories.tsx
@@ -32,7 +32,7 @@ const Categories = () => {
     if (memoizedCategories.length < 2) {
       dispatch(setAllCategories(newSetData));
     }
-  }, [data, dispatch,memoizedCategories.length,newSetData]);
+  }, [data, dispatch,memoizedCategories.length]);
 
   const handleCategory = (category: string, index: number) => {
     localStorage.setItem("categoryIndex", JSON.stringify(index));
